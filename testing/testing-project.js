acada02cad415f18fda20ed0164e64a9973f410d
JonDm07 <jan.drmota1@gmail.com>
2023-11-19 14:41:37 +0100

    Finish 3th, 4th and 5th assingment

--- testing/testing-project.js
+++ testing/testing-project.js
@@ -14,3 +14,59 @@ export function reverseString(string) {
 
   return reversedArray.join("");
 }
+
+export const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
+
+export function cipher(string, shiftFactor) {
+  let array = string.split("");
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let charCode = array[i].charCodeAt();
+
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)) {
+      charCode += shiftFactor;
+      if (charCode < 90 && charCode > 97) {
+        charCode = 65;
+      } else if (charCode > 122) {
+        charCode = 97;
+      }
+    }
+    newArray.push(String.fromCharCode(charCode));
+  }
+  return newArray.join("");
+}
+
+export function analyzeArray(array) {
+  const sum = array.reduce((accumulator, current) => {
+    return accumulator + current;
+  });
+
+  const average = sum / array.length;
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+
+  return { average, min, max, length: array.length };
+}
+
+[2, 4, 6, 8];
+
+{
+  average: 5;
+  min: 2;
+  max: 8;
+  length: 4;
+}
--- testing/testing-project.test.js
+++ testing/testing-project.test.js
@@ -1,32 +1,65 @@
-import { capitalize, reverseString } from "./testing-project";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  cipher,
+  analyzeArray,
+} from "./testing-project";
 
 test("capitalize", () => {
   expect(capitalize("string")).toBe("String");
-});
-
-test("capitalize", () => {
   expect(capitalize("HEllo")).toBe("Hello");
-});
-
-test("capitalize", () => {
   expect(capitalize("123")).toBe("123");
-});
-test("capitalize", () => {
   expect(capitalize("Hello World")).toBe("Hello world");
 });
 
 test("reverse", () => {
   expect(reverseString("ab")).toBe("ba");
-});
-
-test("reverse", () => {
   expect(reverseString("String")).toBe("gnirtS");
-});
-
-test("reverse", () => {
   expect(reverseString("123")).toBe("321");
+  expect(reverseString("ABC DFG")).toBe("GFD CBA");
 });
 
-test("reverse", () => {
-  expect(reverseString("ABC DFG")).toBe("GFD CBA");
+test("add", () => {
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.add(10, 10)).toBe(20);
+});
+
+test("subtract", () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+  expect(calculator.subtract(15, 10)).toBe(5);
+});
+
+test("divide", () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+  expect(calculator.divide(8, 4)).toBe(2);
+});
+
+test("multiply", () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+  expect(calculator.multiply(6, 0)).toBe(0);
+});
+
+test("cipher", () => {
+  expect(cipher("aa", 1)).toBe("bb");
+  expect(cipher("AAbbCC", 2)).toBe("CCddEE");
+  expect(cipher("defend the east wall of the castle", 1)).toBe(
+    "efgfoe uif fbtu xbmm pg uif dbtumf"
+  );
+  expect(cipher(`"Hello there"`, 2)).toBe(`"Jgnnq vjgtg"`);
+});
+
+test("analyze", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+  expect(analyzeArray([2, 4, 6, 8])).toEqual({
+    average: 5,
+    min: 2,
+    max: 8,
+    length: 4,
+  });
 });
