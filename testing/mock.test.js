67fb13c527378a8215af6317b68178319a210263
JonDm07 <jan.drmota1@gmail.com>
2024-01-28 22:33:12 +0100

    Add css folder, testing folder

---
+++ testing/mock.test.js
@@ -0,0 +1,15 @@
+import { forEach } from "./mock";
+
+const mockCallback = jest.fn((x) => 42 * x);
+
+test("forEach mock function", () => {
+  forEach([0, 1], mockCallback);
+
+  expect(mockCallback.mock.calls).toHaveLength(2);
+
+  expect(mockCallback.mock.calls[0][0]).toBe(0);
+
+  expect(mockCallback.mock.calls[1][0]).toBe(1);
+
+  expect(mockCallback.mock.results[0].value).toBe(0);
+});
